cc8d1bf475e30eeb128cc9569f2682a29d4551dd
kaxxa123 <axzammit@hotmail.com>
2023-07-11 16:19:22 +0200

    Ethernaut Good Samaritan Setup

---
+++ ethernaut/24_GoodSamaritan/README.md
@@ -0,0 +1,15 @@
+# Good Samaritan
+
+[Good Samaritan](https://ethernaut.openzeppelin.com/level/0x8586Fe7809208B08691A1D225ab2648De02de76B)
+
+_drain all the balance from his Wallet_
+
+
+```BASH
+solc-select install 0.8.19
+solc-select use 0.8.19
+
+# slither ./src/CryptoVault.sol --print contract-summary  \
+#        --solc-remaps "openzeppelin-contracts/=lib/openzeppelin-contracts/contracts/"
+```
+
---
+++ ethernaut/24_GoodSamaritan/src/Wallet.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./Coin.sol";
+
+contract Wallet {
+    // The owner of the wallet instance
+    address public owner;
+
+    Coin public coin;
+
+    error OnlyOwner();
+    error NotEnoughBalance();
+
+    modifier onlyOwner() {
+        if(msg.sender != owner) {
+            revert OnlyOwner();
+        }
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function donate10(address dest_) external onlyOwner {
+        // check balance left
+        if (coin.balances(address(this)) < 10) {
+            revert NotEnoughBalance();
+        } else {
+            // donate 10 coins
+            coin.transfer(dest_, 10);
+        }
+    }
+
+    function transferRemainder(address dest_) external onlyOwner {
+        // transfer balance left
+        coin.transfer(dest_, coin.balances(address(this)));
+    }
+
+    function setCoin(Coin coin_) external onlyOwner {
+        coin = coin_;
+    }
+}
